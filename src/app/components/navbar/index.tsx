2f2e9306a89927fddbe95a164610b09f37fb2bea
Ubiratan Rodrigues <ubiratanrodrigues@Ubiratans-MacBook-Pro.local>
2025-01-31 17:19:03 -0300

    fix(logo): fix logo redirect to main page

--- src/app/components/navbar/index.tsx
+++ src/app/components/navbar/index.tsx
@@ -1,11 +1,14 @@
+import Link from "next/link";
 import { ModeToggle } from "../mode-toggle";
 
 export const Navbar = () => {
   return (
-    <div className="w-full h-80 bg-bannerBg bg-contain bg-center bg-no-repeat relative flex justify-end">
-      <div className="absolute">
-        <ModeToggle />
+    <Link href={`/`}>
+      <div className="w-full h-80 bg-bannerBg bg-contain bg-center bg-no-repeat relative flex justify-end">
+        <div className="absolute">
+          <ModeToggle />
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
